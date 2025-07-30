ad81b1b3a5335b64a6fd27d13935c02009a7c2f0
TRIVENI SURESH GAIDHANE <66106040+trivenisuresh722@users.noreply.github.com>
2025-07-30 18:57:23 +0000

    Changes

--- Practice/03_conversionOperation.js
+++ Practice/03_conversionOperation.js
@@ -1,11 +1,11 @@
 let score = "123"
 
-console.log(typeof score)
-console.log(typeof(score))
+/*console.log(typeof score)
+console.log(typeof(score))*/
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
+/*console.log(typeof valueInNumber)
+console.log(valueInNumber)*/
 
 /**
  * "33" --> 33
@@ -16,7 +16,7 @@ console.log(valueInNumber)
 
 let isLoggedIn = 1
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 
 /**
  * 1 ==> true
@@ -27,5 +27,40 @@ console.log(booleanIsLoggedIn);
 
 let someNumber = 18
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+/*console.log(stringNumber);
+console.log(typeof stringNumber);*/
+
+//***********************Operations In Js************/
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " triveni"
+
+let str3 = str1 + str2
+console.log(str3);
+
+/*console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");*/
+
+/*console.log(+true);
+console.log(+"");*/
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+let gameCounter = 100
+++gameCounter
+console.log(gameCounter)
+let gameCounter2 = 100
+gameCounter2++
+console.log(gameCounter2)
